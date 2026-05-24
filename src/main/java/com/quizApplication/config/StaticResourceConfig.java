d27797467502aaaafd4f40620c0d249b871a1656
Deepak <206105526+Curious-Deepak@users.noreply.github.com>
2026-05-24 23:30:06 +0530

    Updated asset resource handler
    Changed resource location to use absolute path for assets.

--- src/main/java/com/quizApplication/config/StaticResourceConfig.java
+++ src/main/java/com/quizApplication/config/StaticResourceConfig.java
@@ -3,6 +3,8 @@ package com.quizApplication.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 public class StaticResourceConfig implements WebMvcConfigurer {
@@ -10,8 +12,11 @@ public class StaticResourceConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
+        Path uploadDir = Paths.get("uploads/assets");
+        String uploadPath = uploadDir.toFile().getAbsolutePath();
+        
         registry.addResourceHandler("/assets/**")
-                .addResourceLocations("file:uploads/assets/");
+                .addResourceLocations("file:" + uploadPath + "/");
     }
 
 }
